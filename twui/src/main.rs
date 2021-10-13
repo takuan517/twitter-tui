fe071cee2826e5c0bb00956864db816273805ade
boke0 <speken00.tt@gmail.com>
2021-10-14 00:04:14 +0900

    update

--- twui/src/main.rs
+++ twui/src/main.rs
@@ -1,3 +1,22 @@
+extern crate twui;
+extern crate termion;
+
+use std::io::{stdin, stdout};
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use twui::controller::Controller;
+
+
 fn main() {
-    println!("Hello, world!");
+    let std_in = stdin();
+    let mut std_out = stdout().into_raw_mode().unwrap();
+    let mut state = Controller::new(std_out);
+    for evt in std_in.events() {
+        let c = evt.unwrap();
+        state = state.input(c);
+        if state.exit {
+            return;
+        }
+        state.render();
+    }
 }
